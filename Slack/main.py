0b767fd6587558af7de9353582ef63da0d614332
Chetan <126090346+ChetanM1555@users.noreply.github.com>
2023-06-09 16:01:49 +0200

    can now messages to client

--- Slack/main.py
+++ Slack/main.py
@@ -1,48 +1,67 @@
-from slack import WebClient
+from slack_sdk.web import WebClient
 from simple_term_menu import TerminalMenu
 
 # Create a WebClient instance using your API token
-slack_token = "xoxb-5417465273808-5408907098545-rBcgkD4ZWMaFjBsYrignTkco"
+slack_token = "xoxb-5417465273808-5399769040979-RPfmByTYfXPxfoGXlcO8pIMB"
 client = WebClient(token=slack_token)
 
+ans = input(" Do you want to send to a channel or direct message")
+if ans == "c":
 # Call the conversations.list method to retrieve the list of channels
-response = client.conversations_list()
+    response = client.conversations_list()
 
-options = []
+    options = []
 
-# Check if the API call was successful
-if response["ok"]:
-    # Iterate over the list of channels and print their IDs and names
-    # print("------------------------------->") 
-    for channel in response["channels"]:
-        channel_id = channel["id"]
-        channel_name = channel["name"]
-        choice = f"{channel_name} - {channel_id}"
-        options.append(choice)
-        # print("Channel Name:", channel_name)
-        # print("Channel ID:", channel_id)
-        # print("------------------------------->")
+    # Check if the API call was successful
+    if response["ok"]:
+        # Iterate over the list of channels and print their IDs and names
+        print("------------------------------->") 
+        for channel in response["channels"]:
+            channel_id = channel["id"]
+            channel_name = channel["name"]
+            choice = f"{channel_name} = {channel_id}"
+            options.append(choice)
+
+    else:
+        print("Failed to retrieve channels. Error:", response["error"])
+
 else:
-    print("Failed to retrieve channels. Error:", response["error"])
+    response = client.users_list()
+
+    options = []
+
+    if response["ok"]:
+        
+        for user in response["members"]:
+            user_id = user["id"]
+            user_name = user["real_name"] or user["name"]
+            choice = f"{user_name} = {user_id}"
+            options.append(choice)
+
+
+
+print(options)
+
 
 menu = TerminalMenu(options)
-menu.show()
-
-selected_index = menu.selected_option
+selected_option = menu.show()
 
 # Handle user selection
-selected_option = options[selected_index]
 print("Selected option:", selected_option)
+selected_option = options[selected_option]
 
 # Define the channel and message you want to send
-channel_id = "C05BYBR2FAM"  # Replace with the actual channel ID
-message = "Testing!!!!"
+a = selected_option.split(" = ")
 
-# # Send the message
-# response = client.chat_postMessage(channel=channel_id, text=message)
+channel_id = a[1]  # Replace with the actual channel ID
+message = input("Enter a message: ")
 
-# # Check if the message was sent successfully
-# if response["ok"]:
-#     print("Message sent successfully!")
-# else:
-#     print("Failed to send message. Error: ", response["error"])
+
+# Send the message
+response = client.chat_postMessage(channel=channel_id, text=message)
+
+# Check if the message was sent successfully
+if response["ok"]:
+    print("Message sent successfully!")
+else:
+    print("Failed to send message. Error: ", response["error"])
