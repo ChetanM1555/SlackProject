3ecfe9c930b3562d153e4a5d52bfcb47c4b41e10
Chetan <126090346+ChetanM1555@users.noreply.github.com>
2023-06-12 16:11:24 +0200

    Added input text for token

--- Slack/main.py
+++ Slack/main.py
@@ -2,12 +2,13 @@ from slack_sdk.web import WebClient
 from simple_term_menu import TerminalMenu
 
 # Create a WebClient instance using your API token
-slack_token = "xoxb-5417465273808-5399769040979-RPfmByTYfXPxfoGXlcO8pIMB"
+slack_token = input("copy and paste your slack token here: ")
+# slack_token = "xoxb-5417465273808-5399769040979-RPfmByTYfXPxfoGXlcO8pIMB"
 client = WebClient(token=slack_token)
 
 ans = input(" Do you want to send to a channel or direct message")
 if ans == "c":
-# Call the conversations.list method to retrieve the list of channels
+    # Call the conversations.list method to retrieve the list of channels
     response = client.conversations_list()
 
     options = []
@@ -15,7 +16,7 @@ if ans == "c":
     # Check if the API call was successful
     if response["ok"]:
         # Iterate over the list of channels and print their IDs and names
-        print("------------------------------->") 
+        print("------------------------------->")
         for channel in response["channels"]:
             channel_id = channel["id"]
             channel_name = channel["name"]
@@ -31,13 +32,12 @@ else:
     options = []
 
     if response["ok"]:
-        
+
         for user in response["members"]:
             user_id = user["id"]
             user_name = user["real_name"] or user["name"]
             choice = f"{user_name} = {user_id}"
             options.append(choice)
-
 
 
 print(options)
