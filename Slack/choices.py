345f4a8f3167b511dc0b693cff8d36e5995b62e8
Chetan <126090346+ChetanM1555@users.noreply.github.com>
2023-06-29 12:06:25 +0200

    added error handling

--- Slack/choices.py
+++ Slack/choices.py
@@ -1,4 +1,9 @@
-def choose(options2,colors,client, c, ans, options):
+import slack_sdk
+from colors import colors
+
+
+def choose(options2,client, c, ans):
+    options = []
     quit = False
     response = ""
     response2 = ""
@@ -17,18 +22,29 @@ def choose(options2,colors,client, c, ans, options):
 
         
     elif options2[ans] == "Quit":
-        print(colors.BLUE + "Do you want to send to a channel or direct message: " + colors.PINK)
+        # print(colors.BLUE + "Do you want to send to a channel or direct message: " + colors.PINK)
         print(colors.CYAN + "Noice!")
         quit = True
 
     elif options2[ans] == "Create a channel":  
-        print(colors.BLUE + "What do you want to name the channel: " + colors.PINK)
-        myinput = input()
-        response = client.conversations_create(
-        name=myinput,
-        is_private=False )
-        channel = response["channel"]["name"]
-        print(f"{channel} has been created")
+        try:
+            print(colors.BLUE + "What do you want to name the channel: " + colors.PINK)
+            myinput = input()
+            response = client.conversations_create(
+            name=myinput,
+            is_private=False )
+            channel = response["channel"]["name"]
+            print(f"{channel} has been created")
+        
+        except slack_sdk.errors.SlackApiError as e:
+            # Handle the specific error
+            error_message = e.response["error"]
+            if error_message == "invalid_name_specials":
+                # Handle the 'invalid_name_specials' error
+                print(colors.RED+"Invalid name with special characters.")
+            else:
+                # Handle other errors
+                print(colors.RED+"An error occurred:", error_message)
 
     else:
         # Direct message option
--- Slack/main.py
+++ Slack/main.py
@@ -1,4 +1,5 @@
 from slack_sdk.web import WebClient
+import slack_sdk
 from simple_term_menu import TerminalMenu
 from colors import colors
 import choices
@@ -6,63 +7,74 @@ import create_list
 import config
 
 
-# Create a WebClient instance using your API token
-config.install_simple_term()
-while(True):
-    
+def run():
+    while(True):
+        # Create a WebClient instance using your API token
         slack_token = input(colors.BLUE + "Copy and paste your slack token here: \n" + colors.PINK)
         client = WebClient(token=slack_token)
         break
+        
     
+    while (True):
+            try:
+                options2 = ["Channel","Direct Message","Invite Something","Create a channel", "Quit"]
+                options = []
+                
+                menu = TerminalMenu(options2)
+                ans = menu.show()
 
-while (True):
-    options2 = ["Channel","Direct Message","Invite Something","Create a channel", "Quit"]
-    options = []
+                quit,response, response2, options =choices.choose(options2, client, create_list, ans)
+                
+                if quit == True:
+                    break
+                if(options2[ans]!="Create a channel"):
+                    menu = TerminalMenu(options)
+                    selected_option = menu.show()
 
-    
-    menu = TerminalMenu(options2)
-    ans = menu.show()
+                    # Handle user selection
+                    print(colors.CYAN + "Selected option:", options[selected_option])
+                    selected_option = options[selected_option]
 
-    quit,response, response2, options =choices.choose(options2, colors, client, create_list, ans, options)
-    
+                    # Define the channel and message you want to send
+                    a = selected_option.split(" = ")
+                    print(options2[ans])
+                    if options2[ans] == "Invite Something":
+                        print("Where do you want to invite this person?")
+                        response = client.conversations_list()
+                        options=[]
+                        options = create_list.channel_list(options, response)
+                        menu = TerminalMenu(options)
+                        selected_option2 = menu.show()
+                        selected_option2 = options[selected_option2]
+                        selected_option2 = selected_option2.split(" = ")
+                        selected_option = selected_option.split(" = ")
 
-    if quit == True:
-        break
-    if(options2[ans]!="Create a channel"):
-        menu = TerminalMenu(options)
-        selected_option = menu.show()
-
-        # Handle user selection
-        print(colors.CYAN + "Selected option:", options[selected_option])
-        selected_option = options[selected_option]
-
-        # Define the channel and message you want to send
-        a = selected_option.split(" = ")
-        print(options2[ans])
-        if options2[ans] == "Invite Something":
-            print("Where do you want to invite this person?")
-            response = client.conversations_list()
-            options=[]
-            options = c.channel_list(options, response)
-            menu = TerminalMenu(options)
-            selected_option2 = menu.show()
-            selected_option2 = options[selected_option2]
-            selected_option2 = selected_option2.split(" = ")
-            selected_option = selected_option.split(" = ")
-
-            print(selected_option[1])
-            print(selected_option2[1])
-            r = client.conversations_invite(channel= selected_option2[1], users=selected_option[1])
-        else:
-            channel_id = a[1]  # Replace with the actual channel ID
-            message = input(colors.BLUE + "Enter a message: "+colors.PINK)
+                        r = client.conversations_invite(channel= selected_option2[1], users=selected_option[1])
+                        print(colors.BLUE+"Successfully invited to the channel")
+                    else:
+                        channel_id = a[1]  # Replace with the actual channel ID
+                        message = input(colors.BLUE + "Enter a message: "+colors.PINK)
 
 
-            # Send the message
-            response = client.chat_postMessage(channel=channel_id, text=message)
+                        # Send the message
+                        response = client.chat_postMessage(channel=channel_id, text=message)
 
-            # Check if the message was sent successfully
-            if response["ok"]:
-                print(colors.GREEN + "Message sent successfully!")
-            else:
-                print(colors.RED + "Failed to send message. Error: ", response["error"])
+                        # Check if the message was sent successfully
+                        if response["ok"]:
+                            print(colors.GREEN + "Message sent successfully!")
+                        else:
+                            print(colors.RED + "Failed to send message. Error: ", response["error"])
+            except slack_sdk.errors.SlackApiError as e:
+                error_message = e.response["error"]
+                print(colors.RED+"An error occurred:", error_message)
+
+
+#############################################################################
+x = input("Do you want to install simple term(y/n)?")
+if x == "y": 
+    config.install_simple_term()
+
+# try:
+run()
+# except Exception as e:
+#     print("An unexpected error occured, ending program...")
